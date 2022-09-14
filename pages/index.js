31693fdc330da4acd34ffd9cec41017d73d2cf98
Tektronica <ryalho@gmail.com>
2022-09-14 15:12:30 -0700

    Added buttons to dynamically alter map features + not working + TODO: update vector layer using hooks

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,7 @@ import addMarker from '../components/map-container/features/Marker';
 import ShadowBox from '../components/containers/ShadowBox';
 
 import OSM from 'ol/source/OSM';
+import { useState } from 'react';
 
 export default function Home() {
 
@@ -16,23 +17,62 @@ export default function Home() {
   const center = [-122.576164362, 48.135166126];  // whidbey island
 
   // add features to vector layer
-  var features = [];
-  features.push(addMarker(center))
-  features.push(addMarker([-122.5, 48.135166126]))
+
+  // var features = [];
+  // features.push(addMarker(center));
+  // features.push(addMarker([-122.5, 48.135166126]));
+
+  const [features, setFeatures] = useState([addMarker(center)]);
+
+  function handleClick(id) {
+
+    if (id == 'add') {
+      // https://stackoverflow.com/a/54677026/3382269
+      const coords = [-122.5, 48.135166126];
+      setFeatures(oldArray => [...oldArray, addMarker(coords)]);
+
+    } else if (id == 'delete') {
+      console.log('delete was pressed')
+
+    } else if (id == 'move') {
+      console.log('move was pressed')
+    }
+  }
 
   return (
-    <ShadowBox>
+    <>
+      <ShadowBox>
 
-      <MapCanvas view={{ center: center, zoom: 10 }}>
-        <Layers>
-          <Tile source={source} zIndex={0} />
-          <Vector features={features} zIndex={0} />
-        </Layers>
-      </MapCanvas>
+        <MapCanvas view={{ center: center, zoom: 10 }}>
+          <Layers>
+            <Tile source={source} zIndex={0} />
+            <Vector features={features} zIndex={0} />
+          </Layers>
+        </MapCanvas>
 
-    </ShadowBox>
+      </ShadowBox>
+
+      <ShadowBox>
+        <div className="grid gap-x-4 grid-cols-3">
+          <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" onClick={() => handleClick('add')}>
+            Add Next Marker
+          </button>
+
+          <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" onClick={() => handleClick('delete')}>
+            Delete Last Marker
+          </button>
+
+          <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" onClick={() => handleClick('move')}>
+            Move Last Marker
+          </button>
+
+        </div>
+      </ShadowBox>
+    </>
   )
 }
+
+
 
 Home.getLayout = function getLayout(page) {
   return (
