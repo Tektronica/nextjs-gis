c3c2b653d176cf28681d8e0e50fa48a9c07d7267
Tektronica <ryalho@gmail.com>
2022-09-19 11:36:56 -0700

    updated navbar routes

--- components/navigation/NavBar.js
+++ components/navigation/NavBar.js
@@ -21,19 +21,19 @@ const Navbar = () => {
 
             <div className="flex-grow p-4">
                 <div className="pb-4 text-gray-400 hover:text-white">
-                    <Link href='/status'>
+                    <Link href='/'>
                         <a className="flex items-center">
                             <span className="mr-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M5.636 18.364a9 9 0 010-12.728m12.728 0a9 9 0 010 12.728m-9.9-2.829a5 5 0 010-7.07m7.072 0a5 5 0 010 7.07M13 12a1 1 0 11-2 0 1 1 0 012 0z" />
                                 </svg>
                             </span>
-                            Status
+                            Small Examples
                         </a>
                     </Link>
                 </div>
                 <div className="pb-4 text-gray-400 hover:text-white">
-                    <Link href='/about'>
+                    <Link href='/flightpath'>
                         <a className="flex items-center">
                             <span className="mr-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
@@ -42,7 +42,7 @@ const Navbar = () => {
                                         d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
                                 </svg>
                             </span>
-                            About
+                            Flight Path
                         </a>
                     </Link>
                 </div>
