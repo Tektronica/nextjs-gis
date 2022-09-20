1bc6e4fbac92ec53abde517644e4aead48c9947b
Tektronica <ryalho@gmail.com>
2022-09-20 11:38:45 -0700

    json file has float/int values rather than strings + simplified csvToJson script using pandas + had an issue where pandas json dump wouldn't properly structure file + had to import json library and do the export as before this commit + included requirements.txt for pip use

--- pages/api/scripts/csvToJson.py
+++ pages/api/scripts/csvToJson.py
@@ -1,39 +1,35 @@
-import csv
+import pandas as pd
 import json
 
-
-# https://www.geeksforgeeks.org/how-to-read-a-local-text-file-using-javascript/
-# http://www.convertcsv.com/csv-to-json.htm
-
-# Function to convert a CSV to JSON
-# Takes the file paths as arguments
-def make_json(csvFilePath, jsonFilePath):
-    # create a dictionary
-    data = []
-
-    # Open a csv reader called DictReader
-    with open(csvFilePath, encoding='utf-8') as csvf:
-        csvReader = csv.DictReader(csvf)
-
-        # Convert each row into a dictionary
-        # and add it to data
-        for row in csvReader:
-            data.append(row)
-            # data[key] = rows
-
-    # Open a json writer, and use the json.dumps()
-    # function to dump data
-    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf:
-        jsonf.write(json.dumps(data, indent=4))
-
-
-# Driver Code
-
-# Decide the two file paths according to your
-# computer system
 PARENT_DIRECTORY = 'C:/Users/rholle/Documents/projects/tool-scripts'
 csvFilePath = PARENT_DIRECTORY + r'/airports.csv'
 jsonFilePath = PARENT_DIRECTORY + r'/airports.json'
 
+
+# https://www.geeksforgeeks.org/how-to-read-a-local-text-file-using-javascript/
+# http://www.convertcsv.com/csv-to-json.htm
+# https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_json.html
+# https://stackoverflow.com/a/26181135/3382269
+# https://stackoverflow.com/a/19732660/3382269
+# https://stackoverflow.com/a/28390992/3382269
+
+# Function to convert a CSV to JSON
+# Takes the file paths as arguments
+def make_json(csvPath, jsonPath):
+    # build pandas dataFrame from csv file
+    df = pd.read_csv(csvPath)
+
+    # SyntaxError: Unexpected token N is often the result of accidentally returning a NaN in your JSON
+    df = df.fillna('')
+
+    # while pandas can format as json, there is no line break structure
+    jsonData = df.to_dict('records')
+
+    # export dataFrame as json
+    with open(jsonPath, 'w') as f:
+        f.write(json.dumps(jsonData, indent=4))
+
+
 # Call the make_json function
-make_json(csvFilePath, jsonFilePath)
+if __name__ == "__main__":
+    make_json(csvFilePath, jsonFilePath)
